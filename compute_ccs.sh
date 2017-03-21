69427e62e7f10a8385325db6c81ee11a0300f225
Samuel HANOT <shanot@visu1.erc>
2017-03-21 09:42:42 +0100

    added compute_ccs.sh to compute cross correlation coefficients

---
+++ compute_ccs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for EMDB in [0-9]*
+do
+	for f in ${EMDB}/[0-9]*
+	do
+		srun ~/gmconvert_MAX/gmconvert -igmm $f/$(basename $f).txt -imap ${EMDB}/emd_${EMDB}.map -zth $(cat ${EMDB}/cutoff.txt) |tee $f/gmmscore_threshold &
+	done
+done
