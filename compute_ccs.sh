3a5403fa2c837652b7bdcc12550a9f5c893885c5
Samuel HANOT <smlhnt@gmail.com>
2017-04-10 10:30:11 +0200

    update path

--- compute_ccs.sh
+++ compute_ccs.sh
@@ -4,6 +4,6 @@ for EMDB in [0-9]*
 do
 	for f in ${EMDB}/[0-9]*
 	do
-		srun ~/gmconvert_MAX/gmconvert -igmm $f/$(basename $f).txt -imap ${EMDB}/emd_${EMDB}.map -zth $(cat ${EMDB}/cutoff.txt) |tee $f/gmmscore_threshold &
+		srun ~/gmconvert_MAX_SAM/gmconvert -igmm $f/$(basename $f).txt -imap ${EMDB}/emd_${EMDB}.map -zth $(cat ${EMDB}/cutoff.txt) |tee $f/gmmscore_threshold &
 	done
 done
