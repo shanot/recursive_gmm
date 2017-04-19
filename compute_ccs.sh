05d818caf61727c62fcc8f37b5badb8f5f7a3012
Samuel Hanot <samuel.hanot@pasteur.fr>
2017-04-19 17:18:24 +0200

    compute_ccs can now be used to rasterize the map

--- compute_ccs.sh
+++ compute_ccs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for EMDB in [0-9]*
+while read f
 do
-	for f in ${EMDB}/[0-9]*
-	do
-		srun ~/gmconvert_MAX_SAM/gmconvert -igmm $f/$(basename $f).txt -imap ${EMDB}/emd_${EMDB}.map -zth $(cat ${EMDB}/cutoff.txt) |tee $f/gmmscore_threshold &
-	done
-done
+	mrc="$f/$(basename $f).mrc"
+	gmm="$f/$(basename $f).gmm"
+	output="$f/gmmscore_threshold"
+	if [[ ! -f $output ]] || [[ $gmm -nt $output ]]
+	then
+		srun ~/gmconvert_MAX_SAM/gmconvert -igmm $gmm -imap $f/../threshold.map -omap $mrc -zth $(cat $f/../cutoff.txt) |tee $output &
+	else
+		echo "skipping $f"
+	fi
+done < <(find . -type d -regextype posix-awk -regex "./[0-9]+/[0-9]+")
