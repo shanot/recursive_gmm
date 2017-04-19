d2829abfa5b9894e70273ae724f1993666056614
Samuel Hanot <samuel.hanot@pasteur.fr>
2017-04-19 17:22:37 +0200

    fix logic

--- rasterize_all.sh
+++ rasterize_all.sh
@@ -1,11 +1,17 @@
-#!/bin/zsh
-bindir=${0:h}
-for f in [0-9]*/*_imp.txt
+#!/bin/bash
+
+script="$(readlink -f ${BASH_SOURCE[0]})"
+bindir="$(dirname ${script})"
+
+tmpfile=$(mktemp tmp_XXXX.sh)
+for f in [0-9]*/*_imp.gmm
 do
-	if [[ (! -f $f.mrc) || ($f -nt $f.mrc) ]]
+	if [[ ! -f $f.mrc ]] || [[ $f -nt $f.mrc ]]
 	then
-		~/imp-fast/setup_environment.sh python ${bindir}/rasterize.py $f 2.5 $1 &
-	else
-		echo $f.mrc done
+		echo ~/imp-fast/setup_environment.sh python ${bindir}/rasterize.py $f 2.5 $1
 	fi
-done
+done > ${tmpfile}
+
+${bindir}/make_array_from_cmds.sh RASTER 1 0 < ${tmpfile} > test.sh
+chmod +x test.sh
+srun test.sh
