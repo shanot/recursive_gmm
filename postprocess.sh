8813ef5599d83d9a29924b20e7f20d3d38f574bb
Samuel Hanot <samuel.hanot@pasteur.fr>
2017-05-02 10:49:47 +0200

    fix filenames

--- postprocess.sh
+++ postprocess.sh
@@ -8,8 +8,8 @@ then
 fi
 ${bindir}/gmconvert2imp.sh  ${n}/${n}.gmm >  ${n}/${n}_imp.gmm
 
-~/gmconvert_MAX_SAM/gmconvert -igmm ${n}/${n}.gmm -imap threshold.map -omap ${n}/${n}.mrc -zth $(cat cutoff.txt) |tee gmmscore_threshold
+~/gmconvert_MAX_SAM/gmconvert -igmm ${n}/${n}.gmm -imap threshold.map -omap ${n}/${n}.gmm.mrc -zth $(cat cutoff.txt) |tee gmmscore_threshold
 
-e2proc3d.py ${map_name} ${n}/${n}_imp.gmm.mrc.fsc --calcfsc ${n}/${n}_imp.gmm.mrc
-awk '$2<=0.143 && y>0.143{print 1.0/(x+(0.143-y)*($1-x)/($2-y))} {x=$1;y=$2}' <  ${n}/${n}_imp.gmm.mrc.fsc > ${n}/${n}_imp.gmm.resolution
+e2proc3d.py ${map_name} ${n}/${n}.gmm.mrc.fsc --calcfsc ${n}/${n}.gmm.mrc
+awk '$2<=0.143 && y>0.143{print 1.0/(x+(0.143-y)*($1-x)/($2-y))} {x=$1;y=$2}' <  ${n}/${n}.gmm.mrc.fsc > ${n}/${n}.gmm.resolution
 
