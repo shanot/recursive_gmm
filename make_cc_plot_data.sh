ebda95d03a708b33517899f8a792402f9e926820
Samuel Hanot <samuel.hanot@pasteur.fr>
2017-09-08 17:53:27 +0200

    update plots of CCs

--- make_cc_plot_data.sh
+++ make_cc_plot_data.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-for f in */*/gmmscore_threshold
+for d in $(cut -d ' ' -f 1 $1)
 do
-	
-	if (( $(grep -c '^CC' $f)>0 ))  
+	if [[ -d $d ]]
 	then
-		echo $(dirname $f) $(grep '^CC' $f) $(cat $(dirname $f)/../resolution.txt)
+		r=$(cat $d/resolution.txt)
+		molWt=$(grep molWtTheo $d/header/*xml | head -n 1 | cut -d '>' -f2 | cut -d '<' -f 1)
+		echo 0 \"${r}A \#${d}\"
+		for f in $(ls -v $d/[0-9]*/gmmscore_threshold)
+		do
+			if (( $(grep -c '^CC' $f)>0 ))  
+			then
+				echo $(awk -v molWt=${molWt} '/^#Ngauss /{N=$2} /^CC /{print N/molWt,$2} ' $f)
+				#echo $(awk '/^#Ngauss /{N=$2} /^CC /{print N,$2} ' $f)
+			fi
+		done
+		echo -e '\n\n'
 	fi
-done | tee tmp.txt | tr '/' ' ' | sort -g -s -k 2 | sort -g -s -k 5 | awk -v a='' -F '[/ ]' '$1!=old{print a} {printf "%s %s %s \"%sÅ (EMDB #%s)\"\n", $1,$2,$4,$5,$1; old=$1}' | tee data.dat
+done
